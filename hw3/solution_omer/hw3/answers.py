d32ccf32c9150fc63f5290da0fcb070eb1ed087a
omergottlieb <73241411+omergottlieb@users.noreply.github.com>
2021-01-01 09:28:11 +0200

    Part 3
    uploading sections until training

--- hw3/solution_omer/hw3/answers.py
+++ hw3/solution_omer/hw3/answers.py
@@ -8,28 +8,33 @@ math (delimited with $$).
 # ==============
 # Part 1 answers
 
-
 def part1_rnn_hyperparams():
     hypers = dict(
-        batch_size=0,
-        seq_len=0,
-        h_dim=0,
-        n_layers=0,
-        dropout=0,
-        learn_rate=0.0,
-        lr_sched_factor=0.0,
-        lr_sched_patience=0,
+        batch_size=0, seq_len=0,
+        h_dim=0, n_layers=0, dropout=0,
+        learn_rate=0.0, lr_sched_factor=0.0, lr_sched_patience=0,
     )
     # TODO: Set the hyperparameters to train the model.
     # ====== YOUR CODE: ======
-    raise NotImplementedError()
+    
+    hypers['batch_size']        = 512
+    hypers['seq_len']           = 64
+    hypers['h_dim']             = 256
+    hypers['n_layers']          = 5
+    hypers['dropout']           = 0.3
+    hypers['learn_rate']        = 0.001
+    hypers['lr_sched_factor']   = 0.3
+    hypers['lr_sched_patience'] = 10
+
+
+    # raise NotImplementedError()
     # ========================
     return hypers
 
 
 def part1_generation_params():
     start_seq = ""
-    temperature = 0.0001
+    temperature = .0001
     # TODO: Tweak the parameters to generate a literary masterpiece.
     # ====== YOUR CODE: ======
     raise NotImplementedError()
@@ -38,54 +43,23 @@ def part1_generation_params():
 
 
 part1_q1 = r"""
-**Your answer:**
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
 
 """
 
 part1_q2 = r"""
-**Your answer:**
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
 
 """
 
 part1_q3 = r"""
-**Your answer:**
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
 
 """
 
 part1_q4 = r"""
-**Your answer:**
 
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
 
 """
 # ==============
@@ -99,64 +73,30 @@ PART2_CUSTOM_DATA_URL = None
 
 def part2_vae_hyperparams():
     hypers = dict(
-        batch_size=0, h_dim=0, z_dim=0, x_sigma2=0, learn_rate=0.0, betas=(0.0, 0.0),
+        batch_size=0,
+        h_dim=0, z_dim=0, x_sigma2=0,
+        learn_rate=0.0, betas=(0.0, 0.0),
     )
     # TODO: Tweak the hyperparameters to generate a former president.
     # ====== YOUR CODE: ======
-    raise NotImplementedError()
+    hypers['batch_size'] = 16
+    hypers['h_dim']      = 1024
+    hypers['z_dim']      = 64
+    hypers['x_sigma2']   = 0.01
+    hypers['learn_rate'] = 0.0001
+    hypers['betas']      = (0, 0)
+
+
+    # raise NotImplementedError()
     # ========================
     return hypers
 
 
 part2_q1 = r"""
-**Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
 
 """
 
 part2_q2 = r"""
-**Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
-"""
-
-part2_q3 = r"""
-**Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
-"""
-
-part2_q4 = r"""
-**Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
 
 """
 
@@ -170,65 +110,67 @@ PART3_CUSTOM_DATA_URL = None
 
 def part3_gan_hyperparams():
     hypers = dict(
-        batch_size=0,
-        z_dim=0,
-        data_label=0,
-        label_noise=0.0,
+        batch_size=0, z_dim=0,
+        data_label=0, label_noise=0.0,
         discriminator_optimizer=dict(
-            type="",  # Any name in nn.optim like SGD, Adam
+            type='',  # Any name in nn.optim like SGD, Adam
             lr=0.0,
             # You an add extra args for the optimizer here
         ),
         generator_optimizer=dict(
-            type="",  # Any name in nn.optim like SGD, Adam
+            type='',  # Any name in nn.optim like SGD, Adam
             lr=0.0,
             # You an add extra args for the optimizer here
         ),
     )
     # TODO: Tweak the hyperparameters to train your GAN.
     # ====== YOUR CODE: ======
-    raise NotImplementedError()
+    lr = 0.00001
+    hypers['batch_size'] = 4
+    hypers['z_dim'] = 512
+    hypers['data_label'] = 1
+    hypers['label_noise'] = 0.2
+    hypers['discriminator_optimizer']['type'] = 'Adam'
+    hypers['discriminator_optimizer']['lr'] = lr
+    hypers['generator_optimizer']['type'] = 'Adam'
+    hypers['generator_optimizer']['lr'] = lr*5
+
+    # raise NotImplementedError()
     # ========================
     return hypers
 
 
 part3_q1 = r"""
-**Your answer:**
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
 
 """
 
 part3_q2 = r"""
-**Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
+.
 """
 
 part3_q3 = r"""
-**Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
 
 """
 
 # ==============
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
